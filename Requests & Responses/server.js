1fb697cbcb30ea06fb1675083674db053104d680
tZandr <alex_tjernstrom@hotmail.com>
2026-01-22 21:46:45 +0100

    basic routing

--- Requests & Responses/server.js
+++ Requests & Responses/server.js
@@ -7,14 +7,27 @@ const server = http.createServer((req, res) => {
   // set header content type
   res.setHeader("Content-Type", "text/html");
 
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
   // send an html file
-  fs.readFile("./views/index.html", (error, data) => {
+  fs.readFile(path, (error, data) => {
     if (error) {
       console.log(error);
       res.end();
     } else {
       // res.write(data); -- Not needed if only one thing is sent.
-      res.end();
+      res.end(data); // in that case, data can be passed directly to end()
     }
   });
 });
