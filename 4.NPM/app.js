0abd397b3b442a478086aadbfd14f4c6cbe2c5e4
tZandr <alex_tjernstrom@hotmail.com>
2026-01-26 23:08:25 +0100

    ejs view engine

--- 4.NPM/app.js
+++ 4.NPM/app.js
@@ -3,26 +3,31 @@ const express = require("express");
 // express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+
 // listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  // res.send("<p>home</p>"); -- instead, do below:
-  res.sendFile("./views/index.html", { root: __dirname }); // __dirname = current directory
+  // res.send("<p>home</p>");
+  // res.sendFile("./views/index.html", { root: __dirname }); // __dirname = current directory
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
   // res.send("<p>about</p>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  // res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
-// redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 // 404 page
 app.use((req, res) => {
   // uses when no other route matches, aka when it reaches this point of the code.
-  res.status(404).res.sendFile("./views/404.html", { root: __dirname });
+  //res.status(404).res.sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
