9fa7d612f2d999c65cc55a220c942b36fe825a85
tZandr <alex_tjernstrom@hotmail.com>
2026-01-26 23:09:45 +0100

    better comments

--- 4.NPM/app.js
+++ 4.NPM/app.js
@@ -12,13 +12,13 @@ app.listen(3000);
 app.get("/", (req, res) => {
   // res.send("<p>home</p>");
   // res.sendFile("./views/index.html", { root: __dirname }); // __dirname = current directory
-  res.render("index");
+  res.render("index"); // looks for index.ejs in views folder
 });
 
 app.get("/about", (req, res) => {
   // res.send("<p>about</p>");
-  // res.sendFile("./views/about.html", { root: __dirname });
-  res.render("about");
+  // res.sendFile("./views/about.html", { root: __dirname }); //sends the file about.html
+  res.render("about"); // looks for about.ejs in views folder
 });
 
 app.get("/blogs/create", (req, res) => {
